2116a3a87def4317cc9db8e3958351b3ed3ddbef
yunji.feng <732792434@qq.com>
2024-03-28 21:03:59 +0800

    add state machine

--- src/aruco_det/scripts/aruco_det.py
+++ src/aruco_det/scripts/aruco_det.py
@@ -16,28 +16,20 @@ class Algorithm:
     def __init__(self):
         rospy.init_node("arcuo_det")
         self.debug = rospy.get_param('~is_debug')
-        self.num = 0
         self.state = Odometry()
 
         # for Aruco
         self.bridge = CvBridge()
         self.arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_50)
         self.arucoParams = cv2.aruco.DetectorParameters_create()
-        self.detect = False
-
-        # self.camera_Matrix = np.array([[404.035450, 0., 329.188263],
-        #                                [0.,403.653476, 248.486988,],
-        #                                [0., 0., 1. ]])
+        
         self.camera_Matrix = np.array([[1.78634438e+03,0.00000000e+00,9.13022592e+02],
                                         [0.00000000e+00,1.82753404e+03,4.87987317e+02],
                                         [0.00000000e+00,0.00000000e+00,1.00000000e+00]])
 
-        # self.distortion_Matrix = np.array([0.043602, -0.064718,
-        #                             -0.000313, -0.000471,
-        #                             0.])
         self.distortion_Matrix = np.array([4.51202586e-01,-2.62210378e+00,-5.92390289e-02,2.86948250e-03,4.59687027e+00])
 
-        self.q_camera = np.array([0, 0.707, -0.707, 0])
+        self.q_camera = np.array([0.707, 0, 0, -0.707])
 
         # Subscriber
         pose_sub = rospy.Subscriber('/Odometry', Odometry, self.pose_callback)
@@ -46,7 +38,6 @@ class Algorithm:
         # Publisher 
         self.camera_pub = rospy.Publisher('/aruco_det/vis', Image, queue_size=1)
         self.det_vis_pub = rospy.Publisher('/aruco_det/det_vis', Image, queue_size=1)
-        self.detect_state_pub = rospy.Publisher('/aruco_det/state', Bool, queue_size=1)
         self.detect_result = rospy.Publisher('/aruco_det/target_loc', PoseStamped, queue_size=1)
     
     def pose_callback(self, msg):
@@ -70,87 +61,50 @@ class Algorithm:
         return qv[1 : ]
     
     def camera_callback(self, msg):
-        self.num += 1
-        # cv2.imwrite('/home/nuc_12/img/img_'+str(self.num)+'.jpg', msg)
         msg = self.bridge.imgmsg_to_cv2(msg)
         time_received = rospy.Time.now()
-        # vis_msg = self.bridge.cv2_to_imgmsg(msg)
-        # self.camera_pub.publish(vis_msg)
         now_state = self.state
         cv_image = msg
         (corners, ids, rejected) = cv2.aruco.detectMarkers(cv_image, self.arucoDict,
             parameters=self.arucoParams)
-        
-        # corners = [np.array([[[ 835.,  243.],
-        #                     [1070.,  226.],
-        #                     [1086.,  464.],
-        #                     [ 852.,  481.]]], dtype='float32')]
-
-        flag = Bool()
 
         if len(corners) > 0:
-            print('ids: ', ids)
-            print("corners: ", corners)
             if ids[0][0] == 19:
                 length = 0.2
             else:
                 length = 0.025
-            self.detect_state_pub.publish(True)
-            draw_det_marker_img = cv2.aruco.drawDetectedMarkers(cv_image, corners, ids)
-            # tmp = np.array([[-960, -540],[-960, -540],[-960, -540],[-960, -540]])
-
-            # rvec, tvec, _ = cv2.aruco.estimatePoseSingleMarkers(corners+tmp, length, self.camera_Matrix,np.array([]))
+                
             rvec, tvec, _ = cv2.aruco.estimatePoseSingleMarkers(corners, length, self.camera_Matrix, self.distortion_Matrix)
-            print("rvex.shape ", rvec.shape[0])
             for i in range(rvec.shape[0]):
-                draw_det_marker_img = cv2.drawFrameAxes(draw_det_marker_img, self.camera_Matrix, self.distortion_Matrix,
-                                                              rvec[i, :, :], tvec[i, :, :], 0.03)
-                # draw_det_marker_img = cv2.aruco.drawAxis(draw_det_marker_img, self.camera_Matrix, self.distortion_Matrix,
-                #                                          rvec[i, :, :], tvec[i, :, :], 0.03)
-                self.det_vis_pub.publish(self.bridge.cv2_to_imgmsg(draw_det_marker_img))
                 
                 target_loc = PoseStamped()
                 target_loc.header.stamp = time_received
-                
-                # target_loc.pose.position.x = tvec[0][0][0]
-                # target_loc.pose.position.y = tvec[0][0][1]
-                # target_loc.pose.position.z = tvec[0][0][2]
 
                 pos_camera = np.array([tvec[0][0][0],
                                        tvec[0][0][1],
-                                       tvec[0][0][2]])
-                # print(ids[0][0])
-                # if ids[0][0] == 43:
-                print(pos_camera)
+                                       -tvec[0][0][2]])
+                pos_in_drone = self.quat_rot_vector(self.q_camera, pos_camera)
+                q_drone = np.array([now_state.pose.pose.orientation.w,
+                                    now_state.pose.pose.orientation.x,
+                                    now_state.pose.pose.orientation.y,
+                                    now_state.pose.pose.orientation.z])
                 target_loc.pose.position.x, target_loc.pose.position.y, target_loc.pose.position.z = \
-                    self.quat_rot_vector(self.q_camera, pos_camera)
-                print(target_loc)
-
-                # target_loc.pose.orientation.x = atan(tvec[0][0][0] / tvec[0][0][2])
-                # target_loc.pose.orientation.y = atan(tvec[0][0][1] / tvec[0][0][2])
+                    self.quat_rot_vector(q_drone, pos_in_drone)
+                target_loc.pose.position.x += now_state.pose.pose.position.x
+                target_loc.pose.position.y += now_state.pose.pose.position.y
+                target_loc.pose.position.z += now_state.pose.pose.position.z
 
                 self.detect_result.publish(target_loc)
-
-                flag.data = True
-                self.detect_state_pub.publish(flag)
-
+                if self.debug:
+                    draw_det_marker_img = cv2.aruco.drawDetectedMarkers(cv_image, corners, ids)
+                    draw_det_marker_img = cv2.drawFrameAxes(draw_det_marker_img, self.camera_Matrix, self.distortion_Matrix,
+                                                                rvec[i, :, :], tvec[i, :, :], 0.03)
+                    self.det_vis_pub.publish(self.bridge.cv2_to_imgmsg(draw_det_marker_img))
                 break
-            
-        else:
-            flag.data = False
-            self.detect_state_pub.publish(flag)
-
 
 if __name__ == "__main__":
 
     main_algorithm = Algorithm()
     rate = rospy.Rate(10)
-    # cap = cv2.VideoCapture(0)
     while not rospy.is_shutdown():
-        # retval, frame = cap.read()
-        # # main_algorithm.camera_callback(frame)
-        # if retval:
-        #     main_algorithm.camera_callback(frame)
-        # if cv2.waitKey(5) >= 0:
-        #     break
         rate.sleep()
---
+++ src/state_machine/scripts/state_machine.py
@@ -0,0 +1,184 @@
+#! /usr/bin/env python3
+#coding=utf-8
+
+import rospy
+from geometry_msgs.msg import PoseStamped
+from mavros_msgs.srv import CommandBool
+from mavros_msgs.msg import State
+from nav_msgs.msg import Odometry
+import time
+
+class Algorithm:
+    
+    def __init__(self):
+        rospy.init_node("arcuo_det")
+        self.load_params()
+        self.odom = Odometry()
+        self.mavros_state = State()
+        self.aruco_pose = PoseStamped()
+        self.target_point = PoseStamped()
+        self.car_detected = False
+        self.aruco_detected = False
+
+        self.debug_jump_to = 11
+        
+        '''
+        0: Arming
+        1: Taking off
+        2: Acrossing the 1st square
+        3: Going to the 2nd square
+        4: Acrossing the 2nd square
+        5: Going to the car
+        6: Recognizing the car
+        7: Going to land
+        8: Recognizing the Aruco code
+        9: Landing
+        10: Disarming
+        11: Finish
+        '''
+        self.state = 0
+
+        self.odom_sub = rospy.Subscriber("/mavros/local_position/odom", Odometry, self.odom_callback)
+        self.aruco_sub = rospy.Subscriber("/aruco_det/target_loc", PoseStamped, self.aruco_callback)
+        self.mavros_state_sub = rospy.Subscriber("/mavros/state", State, self.mavros_state_callback)
+
+        self.arming_client = rospy.ServiceProxy("/mavros/cmd/arming", CommandBool)
+
+        self.target_point_pub = rospy.Publisher("/target_point", PoseStamped, queue_size=10)
+    
+    def odom_callback(self, msg):
+        self.odom = msg
+    
+    def aruco_callback(self, msg):
+        self.aruco_pose = msg
+    
+    def mavros_state_callback(self, msg):
+        self.mavros_state = msg
+    
+    def is_close(self, odom: Odometry, pose: list):
+        if abs(odom.pose.pose.position.x - pose[0]) < 0.1 and abs(odom.pose.pose.position.y - pose[1]) < 0.1 and abs(odom.pose.pose.position.z - pose[2]) < 0.1:
+            return True
+        return False
+
+    def go_to(self, pose: list):
+        self.target_point.pose.position.x = pose[0]
+        self.target_point.pose.position.y = pose[1]
+        self.target_point.pose.position.z = pose[2]
+        self.target_point_pub.publish(self.target_point)
+
+    def run(self):
+        # Arming
+        if self.state == 0:
+            if self.mavros_state.armed == True or self.debug_jump_to > 0:
+                self.state = 1
+                return
+            self.arming_client.call(True)
+            return
+        
+        # Taking off
+        elif self.state == 1:
+            if self.debug_jump_to > 1 or self.is_close(self.odom.pose.pose.position, self.takeoff_point):
+                self.state = 2
+                return
+            else:
+                self.go_to(self.takeoff_point)
+                return 
+        
+        # Acrossing the 1st square
+        elif self.state == 2:
+            if self.debug_jump_to > 2 or self.is_close(self.odom.pose.pose.position, self.first_square_point):
+                self.state = 3
+                return
+            else:
+                self.go_to(self.first_square_point)
+                return
+        
+        # Going to the 2nd square
+        elif self.state == 3:
+            if self.debug_jump_to > 3 or self.is_close(self.odom.pose.pose.position, self.second_square_point_pre):
+                self.state = 4
+                return
+            else:
+                self.go_to(self.second_square_point_pre)
+                return
+        
+        # Acrossing the 2nd square
+        elif self.state == 4:
+            if self.debug_jump_to > 4 or self.is_close(self.odom.pose.pose.position, self.second_square_point_end):
+                self.state = 5
+                return
+            else:
+                self.go_to(self.second_square_point_end)
+                return
+        
+        # Going to the car
+        elif self.state == 5:
+            if self.debug_jump_to > 5 or self.is_close(self.odom.pose.pose.position, self.car_point):
+                self.state = 6
+                return
+            else:
+                self.go_to(self.car_point)
+                return
+        
+        # Recognizing the car
+        elif self.state == 6:
+            if self.debug_jump_to > 6 or self.car_detected == True:
+                self.state = 7
+                return
+            return
+        
+        # Going to land
+        elif self.state == 7:
+            if self.debug_jump_to > 7 or self.is_close(self.odom.pose.pose.position, self.land_point):
+                self.state = 8
+                return
+            else:
+                self.go_to(self.land_point)
+                return
+            
+        # Recognizing the Aruco code
+        elif self.state == 8:
+            if self.debug_jump_to > 8 or self.aruco_detected == True:
+                self.state = 9
+                return
+            return
+        
+        # Landing
+        elif self.state == 9:
+            if self.debug_jump_to > 9 or self.odom.pose.pose.position.z < 0.1:
+                self.state = 10
+                return
+            else:
+                self.go_to(self.aruco_pose)
+                return
+        
+        # Disarming
+        elif self.state == 10:
+            if self.debug_jump_to > 10 or self.mavros_state.armed == False:
+                self.state = 11
+                return
+            self.arming_client.call(False)
+            return
+        
+        # Finish
+        elif self.state == 11:
+            return
+
+    def load_params(self):
+
+        self.debug = rospy.get_param('~is_debug')
+        self.takeoff_point = rospy.get_param('~takeoff')
+        self.first_square_point = rospy.get_param('~1st_square')
+        self.second_square_point_pre = rospy.get_param('~2nd_square_pre')
+        self.second_square_point_end = rospy.get_param('~2nd_square_end')
+        self.car_point = rospy.get_param('~car')
+        self.land_point = rospy.get_param('~land')
+        print(type(self.land_point))
+
+if __name__ == "__main__":
+
+    main_algorithm = Algorithm()
+    rate = rospy.Rate(10)
+    while not rospy.is_shutdown():
+        main_algorithm.run()
+        rate.sleep()
